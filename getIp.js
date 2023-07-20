e9afe9f451eba2d5fa8cd6d36307ab0c55bc6184
xv915 <sauheodocuments@gmail.com>
2023-07-20 12:09:23 +0700

    inject ip.html to index.html and homepage

---
+++ getIp.js
@@ -0,0 +1,13 @@
+fetch('./ip.html')
+  .then(response => response.text())
+  .then(ip => {
+    const homepageLink = document.getElementById('homepageLink');
+    homepageLink.href = ip.trim();
+    homepageLink.textContent = 'Go to XV915 Homepage';
+  })
+  .catch(error => {
+    console.error('Error fetching content:', error);
+    const homepageLink = document.getElementById('homepageLink');
+    homepageLink.href = '#';
+    homepageLink.textContent = 'Failed to load XV915 Homepage URL.';
+  });
