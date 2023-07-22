fbe24311a030ba40fe2a8d12f6071fc464d1d025
xv915 <sauheodocuments@gmail.com>
2023-07-22 14:58:36 +0700

    one more try for index.html

--- updateIndexFromBuildDir.js
+++ updateIndexFromBuildDir.js
@@ -1,36 +1,43 @@
 // BY GPT, I HOLD NO CREDIT
-const fs = require('fs');
-const path = require('path');
-const fse = require('fs-extra');
 
-const buildFilePath = path.join(__dirname, './build/index.html');
-const rootFilePath = path.join(__dirname, 'index.html');
+const fs = require("fs");
+const path = require("path");
+const fse = require("fs-extra");
 
-fs.readFile(buildFilePath, 'utf8', (err, data) => {
+const buildFilePath = path.join(__dirname, "./build/index.html");
+const rootFilePath = path.join(__dirname, "index.html");
+
+fs.readFile(buildFilePath, "utf8", (err, data) => {
   if (err) {
-    console.error('Error reading index.html:', err);
+    console.error("Error reading index.html:", err);
     return;
   }
 
   // Modify the href attributes by replacing /sth with /build/sth
-  const modifiedData = data.replace(/(href=")\/(.*?")/g, '$1/build/$2');
+  const modifiedData = data.replace(/(href=")\/(.*?")/g, "$1/build/$2");
 
-  fs.writeFile(buildFilePath, modifiedData, 'utf8', (err) => {
+  // Modify the src attributes by replacing /sth with /build/sth
+  const modifiedDataWithSrc = modifiedData.replace(
+    /(src=")\/(.*?")/g,
+    "$1/build/$2"
+  );
+
+  fs.writeFile(buildFilePath, modifiedDataWithSrc, "utf8", (err) => {
     if (err) {
-      console.error('Error writing modified index.html:', err);
+      console.error("Error writing modified index.html:", err);
       return;
     }
 
-    console.log('index.html modified successfully.');
+    console.log("index.html modified successfully.");
 
     // Copy the modified index.html file to the root directory
     fse.copy(buildFilePath, rootFilePath, (err) => {
       if (err) {
-        console.error('Error copying index.html to root directory:', err);
+        console.error("Error copying index.html to root directory:", err);
         return;
       }
 
-      console.log('index.html copied to the root directory.');
+      console.log("index.html copied to the root directory.");
     });
   });
 });
